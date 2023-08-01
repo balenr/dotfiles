423e67e24b295fe371c24e261515522a71b6aa35
René van Balen <rene.vanbalen@gmail.com>
2023-08-01 14:42:27 +0200

    🔧 Add check if brew is installed
    Add exists function to zshenv

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -1,5 +1,22 @@
-#! /usr/bin/env zsh
+#!/usr/bin/env zsh
 
-echo "\n<<< Starting Homebrew Setup >>>\n"
+echo "\nStarting Homebrew Setup...\n"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if exists brew; then
+    echo "brew exists, skipping install"
+else
+    echo "brew doesn't exist, continuing with install"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+echo "\nInstalling homebrew packages..."
+
+# TODO: Add `--no-quarantine` option to zshrc.
+# You can't use `brew bundle --no-quarantine` as an option.
+# Therefore, use: export HOMEBREW_CASK_OPTS="--no-quarantine" in zshrc.
+
+# TODO: move `brew bundle` line into if statement.
+# If brew isn't installed, then brew bundle shouldn't run.
+brew bundle --verbose
+
+exit 0
