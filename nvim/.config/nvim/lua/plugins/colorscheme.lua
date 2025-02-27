69040881f8452a16fb584ed24eb531f5e9733c5c
René van Balen <rene.vanbalen@gmail.com>
2025-02-27 23:38:15 +0100

    Updates Neovim config

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,16 @@
+local catppuccin = require("plugins.catppuccin")
 return {
   -- Configure LazyVim to load catppuccin
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      theme = catppuccin,
     },
   },
 }
