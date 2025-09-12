68725cdf2b724f5619bbf912306760f0e40b93d8
René van Balen <rene.vanbalen@gmail.com>
2025-09-12 23:03:10 +0200

    New Neovim configuration

---
+++ nvim-jaglinux/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      require("tokyonight").setup({
+        transparent = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      })
+      vim.cmd.colorscheme("tokyonight")
+    end,
+  }
